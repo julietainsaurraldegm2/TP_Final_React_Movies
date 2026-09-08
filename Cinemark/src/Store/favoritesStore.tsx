7ed670c9fad4aab202b6ed78d3cdcb27df092a5b
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-09-08 10:42:15 -0300

    refactor: remove unused API constants and options from favoritesStore

--- Cinemark/src/Store/favoritesStore.tsx
+++ Cinemark/src/Store/favoritesStore.tsx
@@ -1,15 +1,6 @@
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
-export const API_KEY = '8c10e7a4a8f3744b5128e0c32584a906'
-export const API_URL = 'https://api.themoviedb.org/3'
-export const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzEwZTdhNGE4ZjM3NDRiNTEyOGUwYzMyNTg0YTkwNiIsIm5iZiI6MTc4ODQzOTIyMS4yNjksInN1YiI6IjZhOTk2YWI1YjM1NmYwNTNmY2IyZjhjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NqkTZkqmSUuYQ9eFoDjpWuHmuSsxr2cboEvSPdoAipc'
-  }
-}
 
 interface FavoritesState {
   favoriteIds: number[]
