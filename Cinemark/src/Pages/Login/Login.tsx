a5b760807e902aad69a319b3d7a9f8c7b88722f9
Santiago Barua <santiago.barua@gm2dev.com>
2026-09-08 09:41:37 -0300

    feat: update dependencies and fix import paths for userInfo module

--- Cinemark/src/Pages/Login/Login.tsx
+++ Cinemark/src/Pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate  } from 'react-router-dom';
-import userInfo from "../../store/userInfo";
+import userInfo from "../../Store/userInfo";
 
 function Login() {
   const [user, setUser] = useState('')
