2b57c703ff771eb5a362021f1996a36e1ea3d55a
julietainsaurraldegm2 <julieta.insaurralde@gm2dev.com>
2026-09-08 09:45:35 -0300

    fix: correct import path for movies types

--- Cinemark/src/store/UseMoviesStore.tsx
+++ Cinemark/src/store/UseMoviesStore.tsx
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import type { Actor, Movies, Review } from '../types/movies';
+import type { Actor, Movies, Review } from '../Types/movies';
 
 const API_KEY = '8c10e7a4a8f3744b5128e0c32584a906';
 const options = {
